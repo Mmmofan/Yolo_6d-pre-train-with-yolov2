a4539ec5d0c38a6a273d614fe5c58c702d2b55bb
fmo@nullmax.ai <cokespace2@gmail.com>
2019-01-22 15:36:00 +0800

    update

--- train_val.py
+++ train_val.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-# Author:leeyoshinari
-#-----------------------------------------------------------------------------------
+#================================================================
+#   Copyright (C) 2019 * Ltd. All rights reserved.
+#
+#   Editor      : VIM
+#   File name   : fa.py
+#   Author      : Mofan
+#   Created date: 2019-01-21 23:56:12
+#   Description :
+#
+#================================================================
 import tensorflow as tf
 import numpy as np
 import argparse
@@ -27,19 +35,21 @@ class Train(object):
         self.summary_iter = cfg.SUMMARY_ITER
         self.initial_learn_rate = cfg.LEARN_RATE
         self.output_dir = os.path.join(cfg.DATA_DIR, 'output')
-        weight_file = os.path.join(self.output_dir, cfg.WEIGHTS_FILE)
+        self.backup_dir = os.path.join(cfg.DATA_DIR, 'backup')
+        # weight_file = os.path.join(self.output_dir, cfg.WEIGHTS_FILE)
+        # weight_file = os.path.join(self.output_dir, 'yolo_v2.ckpt-3425')
         self.labels_test = None
         self.labels_train = None
 
-        self.variable_to_restore = tf.global_variables()[:-8]
+        # self.variable_to_restore = tf.global_variables()
         self.variable_to_save = tf.global_variables()
-        self.restorer = tf.train.Saver(self.variable_to_restore)
+        # self.restorer = tf.train.Saver(self.variable_to_restore)
         self.saver = tf.train.Saver(self.variable_to_save)
         self.summary_op = tf.summary.merge_all()
         self.writer = tf.summary.FileWriter(self.output_dir)
 
         self.global_step = tf.get_variable('global_step', [], initializer=tf.constant_initializer(0), trainable=False)
-        self.learn_rate = tf.train.exponential_decay(self.initial_learn_rate, self.global_step, 1000, 0.1, name='learn_rate')
+        self.learn_rate = tf.train.exponential_decay(self.initial_learn_rate, self.global_step, 1250, 0.1, name='learn_rate')
         # self.learn_rate = tf.train.piecewise_constant(self.global_step, [100, 190, 10000, 15500], [1e-3, 5e-3, 1e-2, 1e-3, 1e-4])
         self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learn_rate).minimize(self.yolo.total_loss, global_step=self.global_step)
         #self.optimizer = tf.train.GradientDescentOptimizer(learning_rate=self.learn_rate).minimize(self.yolo.total_loss, global_step=self.global_step)
@@ -52,8 +62,8 @@ class Train(object):
         self.sess = tf.Session(config=config)
         self.sess.run(tf.global_variables_initializer())
 
-        print('Restore weights from:', weight_file)
-        self.restorer.restore(self.sess, weight_file)
+        # print('Restore weights from:', weight_file)
+        # self.restorer.restore(self.sess, weight_file)
         self.writer.add_graph(self.sess.graph)
 
     def train(self):
@@ -62,28 +72,29 @@ class Train(object):
 
         num = 1
         initial_time = time.time()
+        lowest_loss = 1e4
 
         for step in xrange(1, self.max_step + 1):
             images, labels = self.data.next_batches(self.labels_train)
             feed_dict = {self.yolo.images: images, self.yolo.labels: labels}
 
             if step % self.summary_iter == 0:
-                if step % 10 == 0:
+                if step % 30 == 0:
+                    print("   Testing... ")
                     self.test()
 
                     summary_, loss, logit, _ = self.sess.run([self.summary_op, self.yolo.total_loss, self.yolo.logits, self.train_op], feed_dict = feed_dict)
                     sum_loss = 0
 
-                    #for i in range(num):
-                    #    images_t, labels_t = self.data.next_batches_test(self.labels_test)
-                    #    feed_dict_t = {self.yolo.images: images_t, self.yolo.labels: labels_t}
-                    #    loss_t = self.sess.run(self.yolo.total_loss, feed_dict=feed_dict_t)
-                    #    sum_loss += loss_t
-
                     log_str = ('{} Epoch: {}, Step: {},\n train_Loss: {:.4f}, test_Loss: {:.4f},\n Remain: {}, learning rate: {}').format(
                         datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), self.data.epoch, int(step),
                         loss, sum_loss/num, self.remain(step, initial_time), self.learn_rate.eval(session=self.sess))
                     print(log_str)
+
+                    if loss < lowest_loss and step > 100:
+                        lowest_loss = loss
+                        print('\n Best model! Save ckpt file to {} '.format(self.output_dir))
+                        self.saver.save(self.sess, self.backup_dir + '/yolo_v2.ckpt', global_step = step)
 
                     if loss < 1e4:
                         pass
@@ -107,7 +118,7 @@ class Train(object):
     def test(self):
         sum_loss = 0
         images, labels = self.data.next_batches_test(self.labels_train)
-        feed_dict = {self.yolo.images: images}
+        feed_dict = {self.yolo.images: images, self.yolo.labels: labels}
         logit, loss_t = self.sess.run([self.yolo.logits, self.yolo.total_loss], feed_dict = feed_dict)
         sum_loss += loss_t
         output = self.softmax(logit, axis=1)
@@ -118,10 +129,42 @@ class Train(object):
             if_true = out_max_idx[0] == lab_max_idx[0]
             if if_true:
                 correct += 1
-        acc = correct / self.batch_size
+        acc = correct / self.yolo.batch_size
 
         print('Epoch: {} in testing, loss: {}, accuracy: {}\n'.format(self.data.epoch, sum_loss, acc))
 
+    def softmax(self, X, theta = 1.0, axis = None):
+        """
+        Compute the softmax of each element along an axis of X.
+        Parameters
+        ----------
+        X: ND-Array. Probably should be floats.
+        theta (optional): float parameter, used as a multiplier
+            prior to exponentiation. Default = 1.0
+        axis (optional): axis to compute values along. Default is the
+            first non-singleton axis.
+        Returns an array the same size as X. The result will sum to 1
+        along the specified axis.
+        """
+        # make X at least 2d
+        y = np.atleast_2d(X)
+        # find axis
+        if axis is None:
+            axis = next(j[0] for j in enumerate(y.shape) if j[1] > 1)
+        # multiply y against the theta parameter,
+        y = y * float(theta)
+        # subtract the max for numerical stability
+        y = y - np.expand_dims(np.max(y, axis = axis), axis)
+        # exponentiate y
+        y = np.exp(y)
+        # take the sum along the specified axis
+        ax_sum = np.expand_dims(np.sum(y, axis = axis), axis)
+        # finally: divide elementwise
+        p = y / ax_sum
+        # flatten if X was 1D
+        if len(X.shape) == 1: p = p.flatten()
+
+        return p
 
     def remain(self, i, start):
         if i == 0:
